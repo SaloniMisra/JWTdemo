0d819937cbeea8e97f30cf72c0a358d7ef68b68e
saloni <saloni.misra@globallogic.com>
2022-09-09 11:52:59 +0530

    role based jwt

---
+++ JWT-Demo/src/main/java/com/example/JWTDemo/model/JwtResponse.java
@@ -0,0 +1,31 @@
+package com.example.JWTDemo.model;
+
+
+public class JwtResponse {
+
+    private String jwtToken;
+
+	public JwtResponse(String jwtToken) {
+		super();
+		this.jwtToken = jwtToken;
+	}
+
+	public JwtResponse() {
+		super();
+	}
+
+	public String getJwtToken() {
+		return jwtToken;
+	}
+
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+
+	@Override
+	public String toString() {
+		return "JwtResponse [jwtToken=" + jwtToken + "]";
+	}
+	
+	
+}
